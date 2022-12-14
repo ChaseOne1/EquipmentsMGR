73213b3d2dba7f6a3cc23d6f673559e4b658588f
ChaseOne1 <chaseone1@qq.com>
2022-12-15 00:12:22 +0800

    完善本地文件框架； 初步构建设备结构体

--- D4B304-EquipmentsMGR/EquipMgr/Equipment.h
+++ D4B304-EquipmentsMGR/EquipMgr/Equipment.h
@@ -1,1 +1,24 @@
 #pragma once
+
+//设备种类
+typedef enum EquipmentType
+{
+	MEC,	//机械类
+	CHM,	//化学类
+	MDC,	//医学类
+	ELC,	//电子类
+	SPC		//特殊设备
+}EquipType;
+
+
+//设备结构体
+typedef struct Equipment
+{
+	EquipType	type;//设备种类
+	char*		name;//设备名称
+	char*		id;//设备编号
+	long long	buy_date;//购入日期
+	long long	scrap_date;//报废日期
+	double		price;//购入价格
+	bool		flag;//报废标志
+}Equip;
