5e7398e2dcb5eb712f47f204e2fcb3af34a99c7f
ChaseOne1 <chaseone1@qq.com>
2022-12-26 23:09:01 +0800

    执行框架构建完成，分类功能完成

--- D4B304-EquipmentsMGR/Main.c
+++ D4B304-EquipmentsMGR/Main.c
@@ -1,6 +1,36 @@
+#include <windows.h>
 #include "Main.h"
+#include "EquipMgr\EquipMgr.h"
+#include "EquipMgr\Equipment.h"
+#include "UserInterface/UserInterface.h"
+
+Equip* equip[5];
+
+void System_Initialize()
+{
+	//初始化
+}
+
+void System_Destory();
 
 int main()
 {
+	SetConsoleOutputCP(65001);
+	System_Initialize();
+	char ctrl;
+	while (true)
+	{
+		SetupUI();
+		printf("> 请输入欲执行的操作： ");
+		ctrl = getchar();
+		if (SystemControl(ctrl))
+			break;
+	}
+	System_Destory();
 	return 0;
+}
+
+void System_Destory()
+{
+	//析构
 }
---
+++ D4B304-EquipmentsMGR/UserInterface/UserInerface.c
@@ -0,0 +1,88 @@
+#include "..\Main.h"
+#include <string.h>
+#include "UserInterface.h"
+#include "..\EquipMgr\EquipMgr.h"
+
+extern const char* cEquipType[TOTAL + 1];
+extern Equip* equip[5];
+
+void SetupUI()
+{
+	system("cls");
+	MainMenuDisplay();
+}
+
+void MainMenuDisplay()
+{
+	printf("--------------设备管理系统------------------\n");
+	printf("|					   |\n");
+	printf("|	  1.添加设备：添加设备信息	   |\n");
+	printf("|	  2.统计设备：分类统计设备	   |\n");
+	printf("|	  3.设备报废：对设备报费处理       |\n");
+	printf("|	  4.设备查询：按条件查询设备       |\n");
+	printf("|	  5.设备排序：按条件排序设备       |\n");
+	printf("|	  6.浏览设备：浏览全部设备         |\n");
+	printf("|	  0.退出系统：退出	           |\n");
+	printf("|					   |\n");
+	printf("--------------------------------------------\n");
+}
+
+bool SystemControl(char ctrl)
+{
+	system("cls");
+	char ch;	getchar();
+	switch (ctrl)
+	{
+	case '1':
+		break;
+	case '2':
+		Count();
+		break;
+	case '3':
+		break;
+	case '4':
+		break;
+	case '5':
+		break;
+	case '6':
+		break;
+	case '0':
+		printf("确认退出？(Y/N)\n");
+		ch = getchar();
+		if (ch == 'Y' || ch == 'y')
+			return true;
+		else
+			break;
+	}
+	return false;
+}
+
+void Count()
+{
+	system("cls");
+	char type[6];	eEquipType TYPE = -1;
+	const char* cEquipType[TOTAL + 1] = { "MEC","CHM","MDC","ELC","SPC","TOTAL" };
+	unsigned char* result = TypeCount(equip, 5);
+	while (true)
+	{
+		printf("请输入欲统计的类别： (机械类:MEC、化学类:CHM、医学类:MDC、电子类:ELC、特殊类:SPC、所有:TOTAL)\n退出请输入N\n> ");
+		gets(type);
+		for (int i = 1; i < TOTAL; ++i)
+		{
+			if (!strcmp(type, cEquipType[i]))
+			{
+				TYPE = i;
+				break;
+			}
+		}
+		if (type[0] == 'N' || type[0] == 'n')
+			break;
+		if (TYPE != -1)
+			printf("%s 设备共有 %d 台（报废 %d 台）\n",
+				cEquipType[TYPE], result[TYPE], result[TYPE + TOTAL]);
+		else
+			printf("请重新输入\n");
+	}
+	for (int i = 0; i < TOTAL * 2; ++i)	free(result);
+	result = NULL;
+}
