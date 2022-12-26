cd2cc1e159f00f1be60053019fb5726ead72201b
XLiu666233 <120567582+XLiu666233@users.noreply.github.com>
2022-12-27 01:37:02 +0800

    还剩一个“写单条信息”
    还剩一个“写单条信息”，抱歉我还是不太明白“写单条信息”究竟是什么意思 [笑哭] ，明天一定问问；排序那部分真的是写了又推翻、一遍遍地补课填坑，然后现在又会冒出一堆的error、也许是工程文件还没有整合、我会好好修改的[捂脸]；抱歉耽误工程的进展了，大家辛苦 [咖啡]

---
+++ D4B304-EquipmentsMGR/UserInterface/UserInerface.c
@@ -0,0 +1,43 @@
+#include "..\Main.h"
+#include <string.h>
+#include <stdlib.h>
+
+
+//在输入所有设备信息之后，设备信息以编号从小到大的顺序进行排序
+/*我在这里耽误了很多时间，最后借鉴了CSDN的代码；
+源代码是以int类型进行排序，而我们的是以char类型数据进行排序，两者似乎不能通用？*/
+int cmp(const void* a,const void* b)
+{
+	Equip *aa=(Equip *)a;
+	Equip *bb=(Equip *)b;
+	return (aa->id)>(bb->id);
+}
+
+void SortbyID(Equip,int n)//n是输入设备的数量、指输入了几个设备
+{
+	qsort(Equip,n,sizeof(Equip[0]),cmp);
+	for(int i=0;i<n;i++)
+	{
+		printf("%s %c %c %d %d %f %d\n",Equip[i].type,Equip[i].name,Equip[i].id,Equip[i].buy_date,Equip[i].scrap_date,Equip[i].price,Equip[i].flag);
+	}
+}
+
+
+
+
+
+/*按设备名称查询*/
+void SearchbyName(int n)//n是输入设备的数量、指输入了几个设备
+{
+	char Name[10];
+	int i;
+	printf("请输入想要查询的设备名称:");
+	scanf("%s",&Name);
+	for(i=0;i<n;i++)
+	{
+		if(strcmp(Name,Equip[i].name)==0)
+			printf("%s %c %c %d %d %f %d\n",Equip[i].type,Equip[i].name,Equip[i].id,Equip[i].buy_date,Equip[i].scrap_date,Equip[i].price,Equip[i].flag);
+	}
+	if(i==n||strcmp(Name,Equip[n].name)!=0)
+		printf("未找到该设备\n");
+}
