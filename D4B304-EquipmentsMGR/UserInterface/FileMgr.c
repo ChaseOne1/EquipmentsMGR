8897031877b912c937e926facd05897692384758
XLiu666233 <120567582+XLiu666233@users.noreply.github.com>
2022-12-29 03:58:48 +0800

    将输入的信息保存到文件，缺指定行写入
    修改了一些细节

--- D4B304-EquipmentsMGR/UserInterface/FileMgr.c
+++ D4B304-EquipmentsMGR/UserInterface/FileMgr.c
@@ -1,28 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
-//设备种类
-typedef enum EquipmentType
-{
-	MEC,	//机械类
-	CHM,	//化学类
-	MDC,	//医学类
-	ELC,	//电子类
-	SPC		//特殊类
-}EquipType;
-
-//设备结构体
-typedef struct Equipment
-{
-	EquipType	type;		//设备种类
-	char 		name;		//设备名称
-	char		id;			//设备编号
-	long long	buy_date;	//购入日期
-	long long	scrap_date;	//报废日期
-	double		price;		//购入价格
-	bool		flag;		//报废标志
-}Equip;
-
 void InputInformation(Equip eqp[],int n);
 void WritetoFile(Equip eqp[], int n);
 void YorN(int *px);
@@ -31,7 +9,6 @@ int main(void)
 {
 	Equip eqp[200];
 	int n,x=0;
-
 	for(;x==0;)
 	{
 		printf("要输入多少台设备?");
@@ -51,13 +28,13 @@ void InputInformation(Equip eqp[],int n)
 	for(i=0;i<n;i++)
 	{
 		printf("请输入一行第%d台的信息: \n",i+1);
-		scanf("%d",		&eqp[i].type) ;
+		scanf("%d",	&eqp[i].type) ;
 		scanf(" %c",	&eqp[i].name);
 		scanf(" %c",	&eqp[i].id); //%c前有一个空格
 		scanf("%lld",	&eqp[i].buy_date);
 		scanf("%lld",	&eqp[i].scrap_date);
 		scanf("%lf",	&eqp[i].price);
-		scanf("%d",		&eqp[i].flag);
+		scanf("%d",	&eqp[i].flag);
 	}
 }
 
@@ -76,19 +53,19 @@ void WritetoFile(Equip eqp[], int n)
 	for(i=0;i<n;i++)
 	{
 		fprintf(fp,"%d %c %c %lld %lld %.2f %d\n",	eqp[i].type,
-													eqp[i].name,
-													eqp[i].id,
-													eqp[i].buy_date,
-													eqp[i].scrap_date,
-													eqp[i].price,
-													eqp[i].flag
+								eqp[i].name,
+								eqp[i].id,
+								eqp[i].buy_date,
+								eqp[i].scrap_date,
+								eqp[i].price,
+								eqp[i].flag
 				);
 	}
 	printf("已输出\n");
 	fclose(fp);
 }
 
-
+//要不要继续
 void YorN(int *px)
 {
 	printf("还要继续输入吗？(Y/N)");
@@ -98,6 +75,6 @@ void YorN(int *px)
 	}
 	else
 	{
-		*px=100;
+		*px=1;
 	}
 }
