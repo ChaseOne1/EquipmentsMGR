400b6de7f84dee03252d1489d52737a3c3564788
YeeChen <2039263760@qq.com>
2022-12-29 21:33:44 +0800

    大部分功能完成： 有序添加到内存、查询、浏览、输入

--- D4B304-EquipmentsMGR/EquipMgr/EquipMgr.h
+++ D4B304-EquipmentsMGR/EquipMgr/EquipMgr.h
@@ -1,9 +1,13 @@
 #pragma once
 
-void InputInfo();
-
-void AddEquip(LinkList* list, Equip* equip)；
+void AddEquip(LinkList* list, Equip* equip);
 
 unsigned char* TypeCount(LinkList* list);
 
 void Date_sort(Equip* equip[]);
+
+LinkList SearchByName(LinkList* list, char* name);
+
+void ScarpEquip(LinkList* list);
+
+bool IsScarp(Equip* equip);
