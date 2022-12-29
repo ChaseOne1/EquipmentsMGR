11692021d164989761d4bd90d99d89c06c6a042e
YeeChen <2039263760@qq.com>
2022-12-29 21:33:44 +0800

    大部分功能完成： 有序添加到内存、查询、浏览、输入

--- D4B304-EquipmentsMGR/Main.c
+++ D4B304-EquipmentsMGR/Main.c
@@ -20,10 +20,35 @@ void System_Initialize()
 	a3->next = a4;
 	a4->next = a5;
 	e1.type = CHM;	e1.flag = true;
+	e1.name = "machao";
+	e1.buy_date = 20220807;
+	e1.id = "D4B001";
+	e1.price = 132.5;
+	e1.scrap_date = 20190611;
 	e2.type = ELC;	e2.flag = true;
+	e2.name = "zhangsan";
+	e2.buy_date = 20221212;
+	e2.id = "D4B003";
+	e2.price = 21.0;
+	e2.scrap_date = 20190427;
 	e3.type = MDC;	e3.flag = true;
+	e3.name = "liangshuo";
+	e3.buy_date = 20220112;
+	e3.id = "D4B004";
+	e3.price = 531.8;
+	e3.scrap_date = 20192121;
 	e4.type = CHM;	e4.flag = false;
+	e4.name = "liuweihao";
+	e4.buy_date = 20220911;
+	e4.id = "D4B005";
+	e4.price = 987.0;
+	e4.scrap_date = 20190228;
 	e5.type = CHM;	e5.flag = false;
+	e5.name = "chenjintao";
+	e5.buy_date = 20221211;
+	e5.id = "D4B006";
+	e5.price = 666.0;
+	e5.scrap_date = 20191111;
 	equipList.head = a1;
 	equipList.tail = a5;
 	equipList.LinkNum = 5;
@@ -52,6 +77,7 @@ void System_Destory()
 	while (curr)
 	{
 		next = curr->next;
+		free(curr->pEquip);
 		free(curr);
 		curr = next;
 	}
